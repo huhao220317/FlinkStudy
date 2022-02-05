5995faf6ecc0d792d369059a8b01ccbc8c483909
huhao220317 <1963518724@qq.com>
2022-02-05 18:13:01 +0800

    自定义数据源，测试专用

---
+++ src/main/java/com/atguigu/day02/ready/Example01.java
@@ -0,0 +1,7 @@
+package com.atguigu.day02.ready;
+
+public class Example01 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/atguigu/day02/ready/MySource.java
+++ src/main/java/com/atguigu/day02/ready/MySource.java
@@ -1,12 +1,21 @@
 package com.atguigu.day02.ready;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
 import java.util.Calendar;
 import java.util.Random;
 
 public class MySource {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.addSource(new ClickEvent())
+                            .print();
+        env.execute();
+    }
 
     public static class ClickEvent implements SourceFunction<JSONObject> {
         private boolean running = true;
