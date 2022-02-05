e07f3d2e50decfc33f2d6bc6ee07c3096e980387
huhao220317 <1963518724@qq.com>
2022-02-05 15:16:21 +0800

    使用底层API实现wc，并理解状态变量

--- src/main/java/com/atguigu/day01/ready/Example01.java
+++ src/main/java/com/atguigu/day01/ready/Example01.java
@@ -1,40 +1,62 @@
 package com.atguigu.day01.ready;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.table.planner.expressions.In;
 import org.apache.flink.util.Collector;
 
 public class Example01 {
-    //这是一个WordCount
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStreamSource<String> dataStreamSource = env
-                .socketTextStream("192.168.232.101", 9999);
-        dataStreamSource.flatMap(new FlatMapFunction<String, String>() {
-            @Override
-            public void flatMap(String value, Collector<String> out) throws Exception {
-                String[] s = value.split(" ");
-                for (String s1 : s) {
-                    out.collect(s1);
-                }
-            }
-        }).map(new MapFunction<String, Tuple2<String,Integer>>() {
+        DataStreamSource<String> ds1 = env.socketTextStream("hadoop101", 9999);
+        SingleOutputStreamOperator<String> process = ds1.map(new MapFunction<String, Tuple2<String,Integer>>() {
             @Override
             public Tuple2<String, Integer> map(String value) throws Exception {
                 return Tuple2.of(value,1);
             }
-        }).keyBy(r->r.f0)
-                .reduce(new ReduceFunction<Tuple2<String, Integer>>() {
-                    @Override
-                    public Tuple2<String, Integer> reduce(Tuple2<String, Integer> value1, Tuple2<String, Integer> value2) throws Exception {
-                        return Tuple2.of(value1.f0,value1.f1+value2.f1);
-                    }
-                }).print();
-        env.execute();
+        }).keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+            @Override
+            public String getKey(Tuple2<String, Integer> value) throws Exception {
+                return value.f0;
+            }
+        })
+                .process(new KeyedProcessFunction<String, Tuple2<String, Integer>, String>() {
+                    private ValueState<Tuple2<String, Integer>> wcStat;
 
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        super.open(parameters);
+                        wcStat = getRuntimeContext().getState(new ValueStateDescriptor<Tuple2<String, Integer>>(
+                                "stat-wc",
+                                Types.TUPLE(Types.STRING,Types.INT)
+
+                        ));
+                    }
+
+                    @Override
+                    public void processElement(Tuple2<String, Integer> stringIntegerTuple2, Context context, Collector<String> collector) throws Exception {
+                        //Tuple2<String, Integer> res = wcStat.value();
+                        if (wcStat.value() == null){
+                            wcStat.update(stringIntegerTuple2);
+                        }else {
+
+                            wcStat.update(Tuple2.of(stringIntegerTuple2.f0,wcStat.value().f1+stringIntegerTuple2.f1));
+                        }
+                        //s输出时使用状态获取值，如果使用res，会出现NullPointException 可能原因是状态更新后res指向并未发生变化
+                        collector.collect(wcStat.value().f0 + " " +  + wcStat.value().f1);
+                    }
+                });
+        process.print();
+        env.execute();
     }
 }
